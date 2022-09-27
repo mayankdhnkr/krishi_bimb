e33890c33a692bf4e65e517408c59c280bbe3005
Mayank <mayankdhankar2002@gmail.com>
2022-09-27 20:00:44 +0530

    basic structure done

---
+++ JS/profile.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("#img-upload");
+
+input.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", function () {
+    const uploaded_image = reader.result;
+    document.querySelector("#placeholder-image").classList.add("hide-image");
+    document.querySelector(
+      "#upload-image"
+    ).style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
